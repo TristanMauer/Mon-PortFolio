8cb53f6e6ddd759a534d3a66ace0b64af260bc27
Tristan Mauer <raykoten@gmail.com>
2023-07-26 22:48:56 +0200

    ajout autorisation entête cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,12 @@ const password = process.env.PASSWORD
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 app.get('/', (req, res) => {
