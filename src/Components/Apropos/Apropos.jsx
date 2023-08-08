3e4b7bab6830f5f55dd81b2cf0a9885267fab7ef
Tristan Mauer <raykoten@gmail.com>
2023-08-08 11:03:42 +0200

    correction texte à propos

--- src/Components/Apropos/Apropos.jsx
+++ src/Components/Apropos/Apropos.jsx
@@ -6,10 +6,9 @@ function Apropos(){
             <div className="apropos_centrage">
             <h1 className="apropos_titre"> A propos</h1>
             <div className="apropos_from_conteneur">
-            <p className="apropos_p"> Depuis mon enfance je suis intéresser par les nouvelles technologies et la culture japonaise. Après une année en licence de psychologie
-            qui c'est solder par un échec j'ai préférer directement travailler afin de prendre du recul sur mes réelles envies. </p>
+            <p className="apropos_p"> Depuis mon enfance je suis intéressé par les nouveaux techniques et la culture japonaise. Après une année en licence de psychologie qui c'est solder par un échec j'ai préféré directement travailler afin de prendre du recul sur mes réelles envies. </p>
             <p className="apropos_p">Fin 2021 je découvre le développement web et j'ai directement 
-            compris que c'est ce qui m'intéresser, décembre 2022 j'ai décider de passer le pas et débuter la formation diplomante d'OpenClassrooms.</p>
+            compris que c'est ce qui m'intéressait, décembre 2022 j'ai décidé de passer le pas et débuter la formation diplômante d'OpenClassRooms.</p>
             <div className="form"> </div>
             </div>
             </div>
